2982c29a52e9c7cdbb49e3e1c61bc508ac370da0
Freek Paans <freekpaans@gmail.com>
2015-07-07 14:20:00 +0200

    missed file

---
+++ NerdDinner/Models/EventScope.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NerdDinner.Models {
+	public class EventScope {
+		[ThreadStatic]
+		static List<Event> _publishedEvents;
+		public static ICollection<Event> Start(Action code) {
+			if(_publishedEvents!=null) {
+				throw new Exception("Cannot nest event scope");
+			}
+			_publishedEvents = new List<Event>();
+			try {
+				code();
+				return _publishedEvents.ToList();
+			}
+			finally {
+				_publishedEvents = null;
+			}
+		}
+
+
+
+		internal static void Raise(Event e) {
+			if(_publishedEvents == null) {
+				throw new Exception("No opened scope");
+			}
+			_publishedEvents.Add(e);
+		}
+	}
+}
